2852b72f8677f7ec02e79caf28959b94d1609647
ajarquin2093 <98927194+ajarquin2093@users.noreply.github.com>
2022-02-05 12:09:57 -0600

    Semana2 Guia1
    Semana2 Guia1

---
+++ Guia1S2/Guia1S2/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guia1S2.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        public IActionResult Borrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Guia1S2/Guia1S2/Controllers/VehiculosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guia1S2.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+
+        public IActionResult Actualizar()
+        {
+            return View();
+        }
+
+
+    }
+}
