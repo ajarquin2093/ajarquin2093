5da2f193160f067183ee72ae469f7388a97ca9df
ajarquin2093 <98927194+ajarquin2093@users.noreply.github.com>
2022-02-22 18:40:14 -0600

    Trabajo En clase
    Semana 4

---
+++ BackEndCapas/BackEnd/BE.API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using BE.DAL.DO.Objetos;
+using BE.DAL.EF;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE.API.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly NDbContext _context;
+
+        public CategoriesController(NDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categories>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+            // return new BE.BS.Categories(_context).GetAll();
+        }
+    }
+}
